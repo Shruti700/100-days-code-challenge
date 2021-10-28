4a201dbb3765d7d99d6074d76ae140578f21fcc0
Shruti pal <63151043+Shruti700@users.noreply.github.com>
2021-10-28 22:00:57 +0530

    Create insertionsort.java

---
+++ insertionsort.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+public class insertionsort{
+    public static void main(String[] args){
+        int[] arr = {15,85,3,65,11,54};
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+ 
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+}
