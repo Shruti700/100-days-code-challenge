45fd7d1af05a6fbb88fe20b8a0a49ad9b6727388
Shruti pal <63151043+Shruti700@users.noreply.github.com>
2021-11-27 11:40:23 +0530

    Update Two pointer.java

--- Two pointer.java
+++ Two pointer.java
@@ -1,3 +1,4 @@
+//Container with most area question
 class Solution {
     public int maxArea(int[] height) {
         int start=0;
@@ -12,5 +13,35 @@ class Solution {
             
         }
         return max;
+    }
+}
+
+-----------------------------------------------------------------------------------------
+    // Tapping rainwater question
+  class Solution {
+    public int trap(int[] height) {
+      int start=0;
+        int end=height.length-1;
+        int lmax=0;
+        int rmax=0;
+        int area=0;
+        while(start<end){
+            
+            
+            if(height[start]>=height[end]) { 
+                 rmax=Math.max(rmax,height[end]);
+               
+                area+=rmax-height[end];
+                 end-=1;
+              }
+            else {
+                lmax=Math.max(lmax,height[start]);
+               
+            area+=lmax-height[start];
+            start+=1;}
+            
+        
+        }
+        return area;  
     }
 }
